1fda0ed3d9d73bbea572f93d3680be787295e013
george bannerman <georgebannerman3@gmail.com>
2026-04-08 16:31:46 -0400

    add app updates and cloud build config

--- app.py
+++ app.py
@@ -5,7 +5,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from CI/CD,We are Live 🚀"
+    return """
+    <html>
+      <head>
+        <title>CI/CD Labb</title>
+      </head>
+      <body style="text-align:center; font-family:Arial; margin-top:40px;">
+        <h1>Hello from CI/CD Labb 🚀</h1>
+        <p>Welcome, it is a beautiful day.</p>
+        <img src="/static/photo.jpg" alt="Photo" width="400">
+      </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
